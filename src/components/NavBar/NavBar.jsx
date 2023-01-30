cecb6ac7d507c7f7560ee4b3020aef8a029c624a
Sumon <m.sumon@outlook.com>
2023-01-30 20:34:58 +0600

    auth

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,10 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { RequireContext } from '../../App';
-import useAuth from '../../pages/Dashboard/hooks/useAuth';
 
 
 const NavBar = () => {
     const { total } = useContext(RequireContext)
-    const { auth } = useAuth();
     const token = localStorage.getItem('accessToken');
 
 
@@ -18,18 +16,18 @@ const NavBar = () => {
         <div className="navbar px-2 py-3 bg-teal-900 text-white">
             <div className="flex-1">
                 {
-                    auth || token ? <Link to="/" className="btn btn-ghost normal-case text-xl">Power Hack</Link> : <Link to="/login" className="btn btn-ghost normal-case text-xl">Power Hack</Link>
+                    token ? <Link to="/" className="btn btn-ghost normal-case text-xl">Power Hack</Link> : <Link to="/login" className="btn btn-ghost normal-case text-xl">Power Hack</Link>
                 }
 
             </div>
             <div className="flex-none">
                 {
-                    auth || token && <h2 className='font-bold mx-14'>Total Paid : {total}</h2>
+                    token && <h2 className='font-bold mx-14'>Total Paid : {total}</h2>
                 }
 
 
                 {
-                    auth || token ? <ul className="menu menu-horizontal px-1 text-lg">
+                    token ? <ul className="menu menu-horizontal px-1 text-lg">
                         <li><Link onClick={handleLogout} className='font-bold' to="">Logout</Link></li>
                     </ul> : <ul className="menu menu-horizontal px-1">
                         <li><button className='font-bold text-lg' to="/login">Login</button></li>
